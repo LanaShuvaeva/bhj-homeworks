88203e84a17a2a9cb87ba3a6925f6f319b23ca95
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2020-05-14 13:46:45 -0700

    menu - second commit

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,45 +1,12 @@
 const menu__links = document.querySelectorAll(".menu__link");
-const menu__subs = document.querySelectorAll(".menu_sub");
 
-
-// console.log(menu__subs[0].classList.add("menu_active"));
-
-menu__links.forEach(menu__link => {
-    const closest = menu__link.closest(".menu_sub");
-    // const dropDown = parent.contains(ul)
-
-
-
-        menu__link.addEventListener("click", function() {
-
-        if (closest !== null) {
-                closest.classList.add("menu_active");
-                console.log("clicked!");
-        }
-        console.log("mooomooo clicked!");
+menu__links.forEach(menu__link => {  
+    menu__link.addEventListener("click", function() {
+        if (!menu__link.nextElementSibling.classList.contains("menu_active")) {
+            document.querySelectorAll(".menu_active").forEach(active => active.classList.remove("menu_active"));
+            menu__link.nextElementSibling.classList.add("menu_active");
+            // return false;
+        } 
     });
-
-
-console.log(closest);
-
+    // return false;
 });
-
-// console.log(menu__links[3].closest(".menu_sub"))
-
-
-
-// const menu__links = document.querySelectorAll(".menu__link");
-// const menu__subs = document.querySelectorAll(".menu__sub");
-
-// menu__links.forEach(menu__link => {
-//     const closest = menu__link.closest(".menu_sub");
-//     if ( closest !== null) {
-//         menu__link.addEventListener("click", function() {
-//             closest.classList.add("menu_active");
-//             console.log("clicked!");
-//         });
-//         console.log(closest);
-//     }
-// });
-
-// console.log(menu__links[3].closest(".menu_sub"))
