b4436e77dd427f51aecf0d6d13395ad76a571d2d
Lana <Shuvaeva.lana@gmail.com>
2020-05-14 19:08:09 -0700

    Menu works on mouseover

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,7 +1,9 @@
+'use strict'
+
 const menu__links = document.querySelectorAll(".menu__link");
 
 menu__links.forEach(menu__link => {  
-    menu__link.addEventListener("click", function() {
+    menu__link.addEventListener("mouseover", function() {
         if (!menu__link.nextElementSibling.classList.contains("menu_active")) {
             document.querySelectorAll(".menu_active").forEach(active => active.classList.remove("menu_active"));
             menu__link.nextElementSibling.classList.add("menu_active");
