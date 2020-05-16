f1c669d520c2656a6b518582524a4de17d34dcb1
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2020-05-16 10:59:38 -0700

    Menu - added oncick func

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -3,12 +3,11 @@
 const menu__links = document.querySelectorAll(".menu__link");
 
 menu__links.forEach(menu__link => {  
-    menu__link.addEventListener("mouseover", function() {
+    menu__link.onclick = () => {
         if (!menu__link.nextElementSibling.classList.contains("menu_active")) {
             document.querySelectorAll(".menu_active").forEach(active => active.classList.remove("menu_active"));
             menu__link.nextElementSibling.classList.add("menu_active");
-            // return false;
+            return false;
         } 
-    });
-    // return false;
+    };
 });
