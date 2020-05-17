6315e7d41e987683b7b2f8497b41e421ab6b7c86
Lana Shuvaeva <shuvaeva.lana@gmail.com>
2020-05-16 18:37:03 -0700

    Dropdown - updated

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -5,8 +5,11 @@ const dropdownList = document.querySelector('.dropdown__list');
 const dropdownItems = Array.from(document.querySelectorAll('.dropdown__item'));
 
 dropdownVal.addEventListener('click', () => {
-    dropdownList.classList.add('dropdown__list_active');
-    return;
+    if (!dropdownList.classList.contains('dropdown__list_active')) {
+        dropdownList.classList.add('dropdown__list_active');
+        return;
+    }
+         dropdownList.classList.remove('dropdown__list_active');
 })
 
 dropdownItems.forEach(item => {
