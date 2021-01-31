455e952475ca6c0fd5f11f15e4267ca30e13aa31
Lana <Shuvaeva.lana@gmail.com>
2021-01-30 22:06:35 -0800

    Preloader - first commit

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -3,9 +3,7 @@ xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
 xhr.responseType = 'json';
 xhr.send();
 
-console.log(document.querySelector('.item'))
-
-function cloneItem (currency, value) {
+function cloneItem(currency, value) {
     let item = document.createElement("div");
     item.setAttribute('class', 'item');
     item.innerHTML += `
@@ -18,13 +16,13 @@ function cloneItem (currency, value) {
             <div class="item__currency">
                 руб.
             </div>
-    ` 
+    `
     document.getElementById('items').appendChild(item);
 }
 
 xhr.addEventListener('readystatechange', () => {
     if (xhr.readyState === xhr.DONE) {
-        for(let charCode in xhr.response.response.Valute) {
+        for (let charCode in xhr.response.response.Valute) {
             let value = xhr.response.response.Valute[charCode].Value;
             cloneItem(charCode, value);
             document.getElementById('loader').classList.remove('loader_active');
