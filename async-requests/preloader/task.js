47aa814beedc47c032cea640b410675bb5896226
Lana <Shuvaeva.lana@gmail.com>
2021-01-30 22:04:00 -0800

    preloader - first commit

---
+++ async-requests/preloader/task.js
@@ -0,0 +1,61 @@
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
+xhr.responseType = 'json';
+xhr.send();
+
+console.log(document.querySelector('.item'))
+
+function cloneItem (currency, value) {
+    let item = document.createElement("div");
+    item.setAttribute('class', 'item');
+    item.innerHTML += `
+            <div class="item__code">
+                ${currency}
+            </div>
+            <div class="item__value">
+                ${value}
+            </div>
+            <div class="item__currency">
+                руб.
+            </div>
+    ` 
+    document.getElementById('items').appendChild(item);
+}
+
+xhr.addEventListener('readystatechange', () => {
+    if (xhr.readyState === xhr.DONE) {
+        for(let charCode in xhr.response.response.Valute) {
+            let value = xhr.response.response.Valute[charCode].Value;
+            cloneItem(charCode, value);
+            document.getElementById('loader').classList.remove('loader_active');
+        }
+    }
+}
+)
+
+
+
+
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
+// xhr.responseType = 'json';
+// xhr.send();
+
+// function cloneItem (currency, value) {
+//     let item = document.querySelector('item'.content).cloneNode(true);
+//     item.querySelector('item__code').textContent = currency;
+//     item.querySelector('item__value').textContent = value;
+//     document.getElementById('items').appendChild(item);
+// }
+
+// xhr.addEventListener('readystatechange', () => {
+//     if (xhr.readyState === xhr.DONE) {
+//         xhr.response.response.Valute.forEach(charCode, () => {
+//             let value = xhr.response.response.Valute[charCode].Value;
+
+//             cloneItem(charCode, value);
+//             document.getElementById('loader').classList.remove('loader_active');
+//         })
+//     }
+// }
+// )
